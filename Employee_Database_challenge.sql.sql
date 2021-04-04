d7c53199c54aaa5aab7b64cab8fe725996514401
Dybondzy <79281335+Dybondzy@users.noreply.github.com>
2021-04-04 15:41:29 -0400

    Add files via upload

---
+++ Employee_Database_challenge.sql.sql
@@ -0,0 +1,71 @@
+
+-- create new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
+
+
+Select * from employees
+Select * from titles
+
+
+Select distinct emp_no, first_name, last_name, birth_date, hire_date
+from employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Select emp_no, title
+from titles
+
+
+Select distinct employees.emp_no, title, first_name, last_name, birth_date, hire_date
+INTO retirement_titles
+from employees 
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+select * 
+from retirement_titles
+
+select count(emp_no) 
+from retirement_titles
+
+select * 
+from titles
+
+select * 
+from employees
+
+
+Select distinct employees.emp_no, first_name, last_name, title, from_date, to_date
+from employees 
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+order by employees.emp_no, title desc;
+
+
+Select distinct employees.emp_no, first_name, last_name, title, from_date, to_date
+INTO retirement_titles
+from employees 
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+order by employees.emp_no, title desc;
